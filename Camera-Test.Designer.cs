5b6a7eda5ad2d4a172a929e10e8c317b013f04d4
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-10-25 23:02:11 +0300

    Changed form background color.
    Changed from color default to Light-Gray.

--- Camera-Test.Designer.cs
+++ Camera-Test.Designer.cs
@@ -28,13 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CameraTest));
             this.SuspendLayout();
             // 
             // CameraTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(554, 277);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "CameraTest";
             this.Text = "Camera Test";
             this.ResumeLayout(false);

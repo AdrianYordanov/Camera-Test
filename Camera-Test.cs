8537b5c3d4f5ffa5704de95dfb6808e6dcaa8c45
Adrian Yordanov <adrian.yordanov.official@gmail.com>
2016-10-26 13:57:56 +0300

    Set the variables that we need.

--- Camera-Test.cs
+++ Camera-Test.cs
@@ -1,17 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Camera_Test
+﻿namespace Camera_Test
 {
+    using System;
+    using System.Drawing;
+    using System.Text;
+    using System.Windows.Forms;
+    // External namespaces.
+    using AForge.Video;
+    using AForge.Video.DirectShow;
+
     public partial class CameraTest : Form
     {
+        private const int defaultImageFileNameLenght = 10;
+        private readonly Random random = new Random((int)DateTime.Now.Ticks);
+        private const string imagesPath = @"..\..\images\";
+
+        private VideoCaptureDevice videoSource;
+        private FilterInfoCollection videoDevices;
+        private Bitmap currentImage;
+        private Bitmap capturedImage;
+
         public CameraTest()
         {
             InitializeComponent();
